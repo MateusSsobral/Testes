c5c40ba91d089f3bd254c57970be4ac6674ce099
MateusSsobral <142116318+MateusSsobral@users.noreply.github.com>
2024-09-30 22:54:21 -0300

    Add files via upload

---
+++ calculadoraSimples.js
@@ -0,0 +1,7 @@
+function calcular(){
+
+    const Valor1 = document.getElementById("v1").value
+    const Valor2 = document.getElementById("v2").value
+    const total = parseInt (Valor1) + parseInt(Valor2) 
+    alert(total)
+   }
